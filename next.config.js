ab1c620881925936b3ac080f68685a5511ea00ca
Abdellah Baqba <abdobaq777@gmail.com>
2026-01-16 15:11:26 +0100

    jjj

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Enable experimental server actions (true is fine)
-  experimental: {
-    serverActions: true,
-  },
 
   // Move external packages out of experimental
   serverExternalPackages: ['mongoose'],
